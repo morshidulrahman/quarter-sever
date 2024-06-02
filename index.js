ef4229f7f5e3c25552629ea7efc33a4a2bf4dc1a
morshidulrahman <boydanger416@gmail.com>
2024-06-02 15:27:41 +0600

    appertment api done

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 app.use(express.json());
 
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.m73tovo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,6 +32,14 @@ const dbConnect = async () => {
   }
 };
 dbConnect();
+
+const Database = client.db("AppertmentDB");
+const appertmentsCollection = Database.collection("appertments");
+
+app.get("/appertments", async (req, res) => {
+  const appertments = await appertmentsCollection.find().toArray();
+  res.json(appertments);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
