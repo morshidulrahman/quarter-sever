c5205f5b74e0ed0dea6e3081bf582b2dcb968047
morshidulrahman <boydanger416@gmail.com>
2024-06-08 01:17:37 +0600

    jwt and andmin-verify

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+
 const app = express();
 app.use(express.json());
 
@@ -12,6 +15,20 @@ const corsOptions = {
   credentials: true,
 };
 app.use(cors(corsOptions));
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.m73tovo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -44,6 +61,23 @@ const CupponsCollection = Database.collection("cuppons");
 const paymentCollection = Database.collection("Payments");
 const paymentInfoCollection = Database.collection("Paymentinfo");
 
+app.post("/jwt", async (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "365d",
+  });
+  res.send({ token: token });
+});
+
+const verifyAdmin = async (req, res, next) => {
+  const user = req.user;
+  const query = { email: user?.email };
+  const result = await usersCollection.findOne(query);
+  if (!result || result?.role !== "admin")
+    return res.status(401).send({ message: "unauthorized access!!" });
+  next();
+};
+
 app.get("/appertments", async (req, res) => {
   const page = parseInt(req.query.page) - 1;
   const size = parseInt(req.query.size);
@@ -75,7 +109,7 @@ app.put("/users", async (req, res) => {
   const result = await usersCollection.updateOne(query, updateDoc, options);
   res.send(result);
 });
-app.get("/admin-stats", async (req, res) => {
+app.get("/admin-stats", verifyToken, verifyAdmin, async (req, res) => {
   const query = { role: "user" };
   const member = { role: "member" };
   const Totaluser = await usersCollection.countDocuments(query);
@@ -126,7 +160,7 @@ app.post("/membersinfo", async (req, res) => {
   res.send(result);
 });
 
-app.get("/members", async (req, res) => {
+app.get("/members", verifyToken, async (req, res) => {
   const result = await membersCollection.find().toArray();
   res.send(result);
 });
@@ -137,7 +171,7 @@ app.get("/member/:email", async (req, res) => {
   res.send(result);
 });
 
-app.delete("/member/:email", async (req, res) => {
+app.delete("/member/:email", verifyToken, verifyAdmin, async (req, res) => {
   const email = req.params.email;
   const query = { email };
   const updateDoc = {
@@ -152,18 +186,13 @@ app.delete("/member/:email", async (req, res) => {
   res.send(result);
 });
 
-app.post("/announcements", async (req, res) => {
+app.post("/announcements", verifyToken, verifyAdmin, async (req, res) => {
   const announcement = req.body;
   const result = await announceCollection.insertOne(announcement);
   res.send(result);
 });
 
-app.get("/announcements", async (req, res) => {
-  const result = await announceCollection.find().toArray();
-  res.send(result);
-});
-
-app.post("/cupon-codes", async (req, res) => {
+app.post("/cupon-codes", verifyToken, verifyAdmin, async (req, res) => {
   const cuppons = req.body;
   const result = await CupponsCollection.insertOne(cuppons);
   res.send(result);
@@ -173,13 +202,13 @@ app.get("/cupon-codes", async (req, res) => {
   const result = await CupponsCollection.find().toArray();
   res.send(result);
 });
-app.get("/cupon/:id", async (req, res) => {
+app.get("/cupon/:id", verifyToken, verifyAdmin, async (req, res) => {
   const id = req.params.id;
   const query = { _id: new ObjectId(id) };
   const result = await CupponsCollection.findOne(query);
   res.send(result);
 });
-app.put("/cupon/:id", async (req, res) => {
+app.put("/cupon/:id", verifyToken, verifyAdmin, async (req, res) => {
   const cupon = req.body;
   const id = req.params.id;
   const query = { _id: new ObjectId(id) };
@@ -200,18 +229,28 @@ app.get("/cupon-codes/:code", async (req, res) => {
 });
 
 // update user status by admin
-app.patch("/agements-user/:email", async (req, res) => {
-  const email = req.params.email;
-  const user = req.body;
-  const query = { email };
-  const updateDoc = {
-    $set: {
-      ...user,
-      timestamp: Date.now(),
-    },
-  };
-  const deletedoc = await agrementsCollection.deleteOne(query);
-  const result = await usersCollection.updateOne(query, updateDoc);
+app.patch(
+  "/agements-user/:email",
+  verifyToken,
+  verifyAdmin,
+  async (req, res) => {
+    const email = req.params.email;
+    const user = req.body;
+    const query = { email };
+    const updateDoc = {
+      $set: {
+        ...user,
+        timestamp: Date.now(),
+      },
+    };
+    const deletedoc = await agrementsCollection.deleteOne(query);
+    const result = await usersCollection.updateOne(query, updateDoc);
+    res.send(result);
+  }
+);
+
+app.get("/announcements", async (req, res) => {
+  const result = await announceCollection.find().toArray();
   res.send(result);
 });
 
@@ -226,7 +265,7 @@ app.post("/agreementlists", async (req, res) => {
   res.json(result);
 });
 
-app.get("/agreementlists", async (req, res) => {
+app.get("/agreementlists", verifyToken, verifyAdmin, async (req, res) => {
   const result = await agrementsCollection.find().toArray();
   res.json(result);
 });
