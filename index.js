ce3bb4a440f7e130c4796acf4d2b3c93cff010c7
morshidulrahman <boydanger416@gmail.com>
2024-06-09 14:27:50 +0600

    cached .env

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://quater-morshidul.netlify.app",
+  ],
   credentials: true,
 };
 app.use(cors(corsOptions));
